5ddeb48246fb53111730bda802185614d234c11c
Corentin BRAVO <corentinbravo@gmail.com>
2021-09-16 14:59:06 +0200

    Added create and list views.

--- EpicEvents/api/urls.py
+++ EpicEvents/api/urls.py
@@ -9,7 +9,21 @@ user_change = UserAPIViewSet.as_view(
     }
 )
 
+user_create = UserAPIViewSet.as_view(
+    {
+        'post': 'create',
+    }
+)
+
+user_list = UserAPIViewSet.as_view(
+    {
+        'get': 'list'
+    }
+)
+
 urlpatterns = [
-    path('users/<int:pk>', user_change, name="user_change"),
+    path('users/<int:pk>/', user_change, name="user_change"),
+    path('users/create/', user_create, name="user_create"),
+    path('users/list/', user_list, name="user_list"),
     path('api-auth/', include('rest_framework.urls'))
     ]
