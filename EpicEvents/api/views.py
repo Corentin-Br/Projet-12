b5552329d07b1435abcdd9fc711383e7e5277067
Corentin BRAVO <corentinbravo@gmail.com>
2021-09-16 12:12:00 +0200

    Added create and update views in the User viewset.

--- EpicEvents/api/views.py
+++ EpicEvents/api/views.py
@@ -1,21 +1,47 @@
 from django.shortcuts import render
-
-# Create your views here.
-from rest_framework.generics import CreateAPIView
+from rest_framework import status
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import MyUserSerializer, ClientSerializer, EventSerializer, ContractSerializer
-from ..accounts.models import MyUser
-from ..clients.models import Contract, Client
-from ..events.models import Event
-from .permissions import IsContactOrReadOnly, IsContactOrSupportOrReadOnly
+from accounts.models import MyUser
+from clients.models import Contract, Client
+from events.models import Event
+from .permissions import IsContactOrReadOnly, IsContactOrSupportOrReadOnly, IsManager
 
 
 class UserAPIViewSet(ModelViewSet):
     queryset = MyUser.objects.all()
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAdminUser, IsManager)
     serializer_class = MyUserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        MyUser.objects.create_user(**serializer.data)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        data = request.data.copy()
+        data["first_name"] = "Jacques"
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        if "password" in serializer.validated_data:
+            serializer.save(password=serializer.validated_data["password"])
+        else:
+            serializer.save()
+
 
 
 class ClientAPIViewSet(ModelViewSet):
