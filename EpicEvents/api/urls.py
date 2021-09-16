7ba67ecafa66378c51f33c6f58eb8dadacff5978
Corentin BRAVO <corentinbravo@gmail.com>
2021-09-16 15:45:12 +0200

    added a delete view.

--- EpicEvents/api/urls.py
+++ EpicEvents/api/urls.py
@@ -17,7 +17,20 @@ user_create = UserAPIViewSet.as_view(
 
 user_list = UserAPIViewSet.as_view(
     {
-        'get': 'list'
+        'get': 'list',
+        'post': 'list'
+    }
+)
+
+# user_find = UserAPIViewSet.as_view(
+#     {
+#         'get': 'retrieve'
+#     }
+# )
+
+user_delete = UserAPIViewSet.as_view(
+    {
+        'post': 'destroy'
     }
 )
 
@@ -25,5 +38,7 @@ urlpatterns = [
     path('users/<int:pk>/', user_change, name="user_change"),
     path('users/create/', user_create, name="user_create"),
     path('users/list/', user_list, name="user_list"),
+    # path('users/<int:pk>/', user_find, name="user_find"),
+    path('users/delete/<int:pk>', user_delete, name="user_delete"),
     path('api-auth/', include('rest_framework.urls'))
     ]
