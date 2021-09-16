147a2cb457af092deb72b936a86a0285f1ca45be
Corentin BRAVO <corentinbravo@gmail.com>
2021-09-16 12:04:09 +0200

    Created the view that updates users.

--- EpicEvents/accounts/admin.py
+++ EpicEvents/accounts/admin.py
@@ -1,10 +1,16 @@
 from django import forms
 from django.contrib import admin
+from django.contrib.admin import helpers
+from django.contrib.admin.exceptions import DisallowedModelAdminToField
+from django.contrib.admin.options import TO_FIELD_VAR, IS_POPUP_VAR
+from django.contrib.admin.utils import unquote, flatten_fieldsets
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
+from django.core.exceptions import PermissionDenied
 
 from .models import MyUser
+from api.urls import user_change
 
 
 class UserCreationForm(forms.ModelForm):
@@ -25,8 +31,11 @@ class UserCreationForm(forms.ModelForm):
         return password2
 
     def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        data = self.cleaned_data.copy()
+        del(data["password1"])
+        del(data["password2"])
+        super().save(commit=False)
+        user = MyUser.objects.create_user(password=self.cleaned_data["password1"], **data)
         if commit:
             user.save()
         return user
@@ -63,6 +72,102 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('email',)
     ordering = ('email',)
     filter_horizontal = ()
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        if request.method == 'POST':
+            response = user_change(request, pk=object_id)
+            if response.status_code == 200:
+                return self.response_change(request, self.get_object(request, object_id))
+            else:
+                context, add, obj = get_context(self, request, object_id, extra_context, api_response=response)
+                return self.render_change_form(request, context, add=add, change=not add, obj=obj, form_url=form_url)
+        else:
+            return super().change_view(request, object_id, form_url, extra_context)
+
+
+def get_context(admin_model, request, object_id, extra_context, api_response):
+    """Create the context required to render the admin template.
+
+    It is needed because django's default change_view will perform change on the database, while we want the API to be
+    the only one to do so."""
+    to_field = request.POST.get(TO_FIELD_VAR, request.GET.get(TO_FIELD_VAR))
+    if to_field and not admin_model.to_field_allowed(request, to_field):
+        raise DisallowedModelAdminToField("The field %s cannot be referenced." % to_field)
+    model = admin_model.model
+    opts = model._meta
+    if '_saveasnew' in request.POST:
+        object_id = None
+
+    add = object_id is None
+
+    if add:
+        if api_response.status_code == 403:
+            raise PermissionDenied
+        obj = None
+    else:
+        obj = admin_model.get_object(request, unquote(object_id), to_field)
+        if api_response.status_code == 403:
+            raise PermissionDenied
+
+    if obj is None:
+        return admin_model._get_obj_does_not_exist_redirect(request, opts, object_id)
+    fieldsets = admin_model.get_fieldsets(request, obj)
+    ModelForm = admin_model.get_form(
+        request, obj, change=not add, fields=flatten_fieldsets(fieldsets)
+    )
+
+    form = ModelForm(request.POST, request.FILES, instance=obj)
+    formsets, inline_instances = admin_model._create_formsets(request, form.instance, change=not add)
+
+    if not add and not admin_model.has_change_permission(request, obj):
+        readonly_fields = flatten_fieldsets(fieldsets)
+    else:
+        readonly_fields = admin_model.get_readonly_fields(request, obj)
+    adminForm = helpers.AdminForm(
+        form,
+        list(fieldsets),
+        # Clear prepopulated fields on a view-only form to avoid a crash.
+        admin_model.get_prepopulated_fields(request, obj) if add or admin_model.has_change_permission(request,
+                                                                                                      obj) else {},
+        readonly_fields,
+        model_admin=admin_model)
+    media = admin_model.media + adminForm.media
+
+    inline_formsets = admin_model.get_inline_formsets(request, formsets, inline_instances, obj)
+    for inline_formset in inline_formsets:
+        media = media + inline_formset.media
+
+    if add:
+        title = 'Add %s'
+    elif admin_model.has_change_permission(request, obj):
+        title = 'Change %s'
+    else:
+        title = 'View %s'
+    context = {
+        **admin_model.admin_site.each_context(request),
+        'title': title % opts.verbose_name,
+        'subtitle': str(obj) if obj else None,
+        'adminform': adminForm,
+        'object_id': object_id,
+        'original': obj,
+        'is_popup': IS_POPUP_VAR in request.POST or IS_POPUP_VAR in request.GET,
+        'to_field': to_field,
+        'media': media,
+        'inline_admin_formsets': inline_formsets,
+        'errors': helpers.AdminErrorList(form, formsets),
+        'preserved_filters': admin_model.get_preserved_filters(request),
+    }
+
+    # Hide the "Save" and "Save and continue" buttons if "Save as New" was
+    # previously chosen to prevent the interface from getting confusing.
+    if "_saveasnew" in request.POST:
+        context['show_save'] = False
+        context['show_save_and_continue'] = False
+        # Use the change template instead of the add template.
+        add = False
+
+    context.update(extra_context or {})
+    return context, add, obj
 
 
 admin.site.register(MyUser, UserAdmin)
