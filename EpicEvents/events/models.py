7dfb8b424d2f9873b42abf42ced79d92a5f87f65
Corentin BRAVO <corentinbravo@gmail.com>
2021-09-22 17:11:21 +0200

    Fixed the date_created to not update when the model is modified.

--- EpicEvents/events/models.py
+++ EpicEvents/events/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Event(models.Model):
     client = models.ForeignKey('clients.Client', on_delete=models.CASCADE, related_name="events")
-    date_created = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     support = models.ForeignKey('accounts.MyUser', on_delete=models.CASCADE, limit_choices_to={'role': 'support'}, related_name="events")
     contract = models.ForeignKey('clients.Contract', on_delete=models.CASCADE, related_name="event")
