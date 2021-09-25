2115e2901a3682bdd318ef283de09f219dad0907
Corentin BRAVO <corentinbravo@gmail.com>
2021-09-25 11:38:14 +0200

    Added tests to check for access to the app, to the user list, to specific users, and for access and success to CRUD operations.

--- EpicEvents/accounts/tests.py
+++ EpicEvents/accounts/tests.py
@@ -1,3 +1,130 @@
-from django.test import TestCase
+import logging
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+from unittest.mock import patch
+
+import accounts.admin
+from accounts.models import MyUser
+
+default_password = "correcthorsebatterystaple"
+
+@patch.object(accounts.admin.file_handler, 'stream', open('debug3.log', 'a'))
+class MyTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.gestion_logs = {"email": "corentin@gmail.com", "password": default_password}
+        cls.sales_logs = {"email": "thomas@gmail.com", "password": default_password}
+        cls.support_logs = {"email": "timothee@gmail.com", "password": default_password}
+        cls.gestion_user = MyUser.objects.create_user(
+            first_name="Corentin",
+            last_name="Bravo",
+            role="gestion",
+            email="corentin@gmail.com",
+            password=default_password
+        )
+        cls.sales_user = MyUser.objects.create_user(
+            first_name="Thomas",
+            last_name="Bravo",
+            email="thomas@gmail.com",
+            role="sales",
+            password=default_password
+        )
+        cls.support_user = MyUser.objects.create_user(
+            first_name="Timothée",
+            last_name="Bravo",
+            email="timothee@gmail.com",
+            role="support",
+            password=default_password
+        )
+        cls.additional_user_data = {
+            "first_name": "Matthieu",
+            "last_name": "Bravo",
+            "email": "matthieu@gmail.com",
+            "role": "support",
+            "password": default_password,
+            "password2": default_password,
+            "_save": "Save"
+        }
+
+    def test_gestion_user_can_access_the_app(self):
+        self.client.login(**self.gestion_logs)
+        resp = self.client.get("/admin/accounts/")
+        assert resp.status_code == 200
+
+    def test_support_and_sales_user_cant_access_the_app(self):
+        for logs in (self.support_logs, self.sales_logs):
+            self.client.login(**logs)
+            resp = self.client.get("/admin/accounts/")
+            assert resp.status_code == 404
+
+    def test_gestion_user_can_access_the_user_list(self):
+        self.client.login(**self.gestion_logs)
+        resp = self.client.get("/admin/accounts/myuser/")
+        assert resp.status_code == 200
+
+    def test_support_and_sales_user_cant_access_the_user_list(self):
+        for logs in (self.support_logs, self.sales_logs):
+            self.client.login(**logs)
+            resp = self.client.get("/admin/accounts/myuser/")
+            assert resp.status_code == 403
+
+    def test_gestion_user_can_access_a_specific_user(self):
+        self.client.login(**self.gestion_logs)
+        for pk in range(1, 4):
+            resp = self.client.get(f"/admin/accounts/myuser/{pk}/change/")
+            assert resp.status_code == 200
+
+    def test_support_and_sales_user_cant_access_a_specific_user(self):
+        for logs in (self.support_logs, self.sales_logs):
+            for pk in range(1, 4):
+                self.client.login(**logs)
+                resp = self.client.get(f"/admin/accounts/myuser/{pk}/change/")
+                assert resp.status_code == 403
+
+    def test_gestion_user_can_modify_a_specific_user(self):
+        self.client.login(**self.gestion_logs)
+        resp = self.client.post("/admin/accounts/myuser/1/change/", {"first_name": "Jacques", "email": self.gestion_user.email, "last_name": self.gestion_user.last_name, "role": self.gestion_user.role, "is_active": self.gestion_user.is_active})
+        assert resp.status_code == 302
+        assert MyUser.objects.get_by_natural_key(self.gestion_user.email).first_name == "Jacques"
+
+    def test_support_and_sales_user_cant_modify_a_specific_user(self):
+        for logs in (self.support_logs, self.sales_logs):
+            self.client.login(**logs)
+            resp = self.client.post("/admin/accounts/myuser/1/change/",
+                                    {"first_name": "Thomas", "email": self.gestion_user.email,
+                                     "last_name": self.gestion_user.last_name, "role": self.gestion_user.role,
+                                     "is_active": self.gestion_user.is_active})
+
+            assert resp.status_code == 403
+            assert MyUser.objects.get_by_natural_key(self.gestion_user.email).first_name == "Corentin"
+
+    def test_gestion_user_can_create_a_user(self):
+        self.client.login(**self.gestion_logs)
+        number_of_users = len(MyUser.objects.all())
+        resp = self.client.post("/admin/accounts/myuser/add/", self.additional_user_data)
+        assert resp.status_code == 302
+        assert len(MyUser.objects.all()) == number_of_users + 1
+
+    def test_support_and_sales_user_cant_create_a_user(self):
+        for logs in (self.support_logs, self.sales_logs):
+            self.client.login(**logs)
+            number_of_users = len(MyUser.objects.all())
+            resp = self.client.post("/admin/accounts/myuser/add/", self.additional_user_data)
+            assert resp.status_code == 403
+            assert len(MyUser.objects.all()) == number_of_users
+
+    def test_gestion_user_can_delete_a_user(self):
+        self.client.login(**self.gestion_logs)
+        number_of_users = len(MyUser.objects.all())
+        resp = self.client.post("/admin/accounts/myuser/2/delete/", {"post": "yes"})
+        assert resp.status_code == 302
+        assert len(MyUser.objects.all()) == number_of_users - 1
+
+    def test_support_and_sales_user_cant_delete_a_user(self):
+        for logs in (self.support_logs, self.sales_logs):
+            self.client.login(**logs)
+            number_of_users = len(MyUser.objects.all())
+            resp = self.client.post("/admin/accounts/myuser/1/delete/", {"post": "yes"})
+            assert resp.status_code == 403
+            assert len(MyUser.objects.all()) == number_of_users
