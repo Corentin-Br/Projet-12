afe397b8663b98b20d3842451188bdfde1656563
Corentin BRAVO <corentinbravo@gmail.com>
2021-09-26 20:19:31 +0200

    Allowed empty notes. Made contract OneToOneField

--- EpicEvents/events/models.py
+++ EpicEvents/events/models.py
@@ -6,10 +6,10 @@ class Event(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     support = models.ForeignKey('accounts.MyUser', on_delete=models.CASCADE, limit_choices_to={'role': 'support'}, related_name="events")
-    contract = models.ForeignKey('clients.Contract', on_delete=models.CASCADE, related_name="event")
+    contract = models.OneToOneField('clients.Contract', on_delete=models.CASCADE, related_name="event")
     attendees = models.PositiveIntegerField()
     date = models.DateTimeField()
-    notes = models.TextField()
+    notes = models.TextField(blank=True, null=True)
 
     @property
     def status(self):
