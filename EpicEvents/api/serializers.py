c7ef000cd3d037af0764aaae32d90b846abc2bfe
Corentin BRAVO <corentinbravo@gmail.com>
2021-09-26 20:20:06 +0200

    Added a validation that the client for the contract must be the same client than for the event.

--- EpicEvents/api/serializers.py
+++ EpicEvents/api/serializers.py
@@ -40,8 +40,14 @@ class EventSerializer(serializers.ModelSerializer):
         model = Event
         fields = ['id', 'client', 'date_created', 'date_updated', 'support', 'contract', 'attendees', 'date', 'notes']
 
+    def validate(self, data):
+        if data["contract"].client != data["client"]:
+            raise serializers.ValidationError("The client must be the same for the event and the contract!")
+        return super().validate(data)
 
 class ContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
         fields = ['id', 'sales_contact', 'client', 'date_created', 'date_updated', 'status', 'amount', 'payment_due']
+
+
