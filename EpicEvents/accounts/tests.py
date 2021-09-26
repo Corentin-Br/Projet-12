132074361fe10a818351f66e6203abfa3f937959
Corentin BRAVO <corentinbravo@gmail.com>
2021-09-26 20:20:49 +0200

    Added tests for unlogged users. Removed direct references to pk, instead made sure they always come from an attribute.

--- EpicEvents/accounts/tests.py
+++ EpicEvents/accounts/tests.py
@@ -1,7 +1,4 @@
-import logging
-
 from django.test import TestCase
-from django.test import Client
 from unittest.mock import patch
 
 import accounts.admin
@@ -10,7 +7,7 @@ from accounts.models import MyUser
 default_password = "correcthorsebatterystaple"
 
 @patch.object(accounts.admin.file_handler, 'stream', open('debug3.log', 'a'))
-class MyTest(TestCase):
+class AccountsTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.gestion_logs = {"email": "corentin@gmail.com", "password": default_password}
@@ -47,6 +44,45 @@ class MyTest(TestCase):
             "_save": "Save"
         }
 
+    def test_unlogged_user_cant_access_the_app(self):
+        resp = self.client.get("/admin/accounts")
+        assert resp.status_code == 302
+        assert resp.url == f"/admin/login/?next={resp.request['PATH_INFO']}"
+
+    def test_unlogged_user_cant_access_the_user_list(self):
+        resp = self.client.get("/admin/accounts/myuser/")
+        assert resp.status_code == 302
+        assert resp.url == f"/admin/login/?next={resp.request['PATH_INFO']}"
+
+    def test_unlogged_user_cant_access_a_specific_user(self):
+        for user in (self.gestion_user, self.sales_user, self.support_user):
+            resp = self.client.get(f"/admin/accounts/myuser/{user.pk}/change/")
+            assert resp.status_code == 302
+            assert resp.url == f"/admin/login/?next={resp.request['PATH_INFO']}"
+
+    def test_unlogged_user_cant_modify_a_specific_user(self):
+        resp = self.client.post(f"/admin/accounts/myuser/{self.gestion_user.pk}/change/",
+                                {"first_name": "Thomas", "email": self.gestion_user.email,
+                                 "last_name": self.gestion_user.last_name, "role": self.gestion_user.role,
+                                 "is_active": self.gestion_user.is_active})
+        assert resp.status_code == 302
+        assert resp.url == f"/admin/login/?next={resp.request['PATH_INFO']}"
+        assert MyUser.objects.get_by_natural_key(self.gestion_user.email).first_name == "Corentin"
+
+    def test_unlogged_user_cant_create_a_user(self):
+        number_of_users = len(MyUser.objects.all())
+        resp = self.client.post("/admin/accounts/myuser/add/", self.additional_user_data)
+        assert resp.status_code == 302
+        assert resp.url == f"/admin/login/?next={resp.request['PATH_INFO']}"
+        assert len(MyUser.objects.all()) == number_of_users
+
+    def test_unlogged_user_cant_delete_a_user(self):
+        number_of_users = len(MyUser.objects.all())
+        resp = self.client.post(f"/admin/accounts/myuser/{self.gestion_user.pk}/delete/", {"post": "yes"})
+        assert resp.status_code == 302
+        assert resp.url == f"/admin/login/?next={resp.request['PATH_INFO']}"
+        assert len(MyUser.objects.all()) == number_of_users
+
     def test_gestion_user_can_access_the_app(self):
         self.client.login(**self.gestion_logs)
         resp = self.client.get("/admin/accounts/")
@@ -71,31 +107,30 @@ class MyTest(TestCase):
 
     def test_gestion_user_can_access_a_specific_user(self):
         self.client.login(**self.gestion_logs)
-        for pk in range(1, 4):
-            resp = self.client.get(f"/admin/accounts/myuser/{pk}/change/")
+        for user in (self.gestion_user, self.sales_user, self.support_user):
+            resp = self.client.get(f"/admin/accounts/myuser/{user.pk}/change/")
             assert resp.status_code == 200
 
     def test_support_and_sales_user_cant_access_a_specific_user(self):
         for logs in (self.support_logs, self.sales_logs):
-            for pk in range(1, 4):
+            for user in (self.gestion_user, self.sales_user, self.support_user):
                 self.client.login(**logs)
-                resp = self.client.get(f"/admin/accounts/myuser/{pk}/change/")
+                resp = self.client.get(f"/admin/accounts/myuser/{user.pk}/change/")
                 assert resp.status_code == 403
 
     def test_gestion_user_can_modify_a_specific_user(self):
         self.client.login(**self.gestion_logs)
-        resp = self.client.post("/admin/accounts/myuser/1/change/", {"first_name": "Jacques", "email": self.gestion_user.email, "last_name": self.gestion_user.last_name, "role": self.gestion_user.role, "is_active": self.gestion_user.is_active})
+        resp = self.client.post(f"/admin/accounts/myuser/{self.gestion_user.pk}/change/", {"first_name": "Jacques", "email": self.gestion_user.email, "last_name": self.gestion_user.last_name, "role": self.gestion_user.role, "is_active": self.gestion_user.is_active})
         assert resp.status_code == 302
         assert MyUser.objects.get_by_natural_key(self.gestion_user.email).first_name == "Jacques"
 
     def test_support_and_sales_user_cant_modify_a_specific_user(self):
         for logs in (self.support_logs, self.sales_logs):
             self.client.login(**logs)
-            resp = self.client.post("/admin/accounts/myuser/1/change/",
+            resp = self.client.post(f"/admin/accounts/myuser/{self.gestion_user.pk}/change/",
                                     {"first_name": "Thomas", "email": self.gestion_user.email,
                                      "last_name": self.gestion_user.last_name, "role": self.gestion_user.role,
                                      "is_active": self.gestion_user.is_active})
-
             assert resp.status_code == 403
             assert MyUser.objects.get_by_natural_key(self.gestion_user.email).first_name == "Corentin"
 
@@ -117,7 +152,7 @@ class MyTest(TestCase):
     def test_gestion_user_can_delete_a_user(self):
         self.client.login(**self.gestion_logs)
         number_of_users = len(MyUser.objects.all())
-        resp = self.client.post("/admin/accounts/myuser/2/delete/", {"post": "yes"})
+        resp = self.client.post(f"/admin/accounts/myuser/{self.sales_user.pk}/delete/", {"post": "yes"})
         assert resp.status_code == 302
         assert len(MyUser.objects.all()) == number_of_users - 1
 
@@ -125,6 +160,6 @@ class MyTest(TestCase):
         for logs in (self.support_logs, self.sales_logs):
             self.client.login(**logs)
             number_of_users = len(MyUser.objects.all())
-            resp = self.client.post("/admin/accounts/myuser/1/delete/", {"post": "yes"})
+            resp = self.client.post(f"/admin/accounts/myuser/{self.gestion_user.pk}/delete/", {"post": "yes"})
             assert resp.status_code == 403
             assert len(MyUser.objects.all()) == number_of_users
