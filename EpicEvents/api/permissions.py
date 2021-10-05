9d1e1ae319f239aa35bf8339dcbce22f4687a279
Corentin BRAVO <corentinbravo@gmail.com>
2021-10-05 18:08:11 +0200

    Removed irrelevant imports, and added comments

--- EpicEvents/api/permissions.py
+++ EpicEvents/api/permissions.py
@@ -1,35 +1,32 @@
-from django.db.models import Q
 from rest_framework import permissions
-from rest_framework.generics import get_object_or_404
-
-import api.views
 
 
 class IsContactOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return request.user.role in ("sales", "support", "gestion")
-        return request.user.role in ("sales", "gestion")
+        return request.user.role in ("sales", "gestion")  # Only users in sales/gestion can edit those objects.
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return request.user.role in ("sales", "support", "gestion")
         return view.queryset.filter(pk=obj.id, sales_contact=request.user).exists() or \
             (view.queryset.filter(pk=obj.id, sales_contact__isnull=True).exists() and request.user.role == "sales") or \
-            request.user.role == "gestion"
+            request.user.role == "gestion"  # Either the user is the sales_contact OR there is no sales_contact OR the user is a gestion user.
 
 
 class IsContactOrSupportOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if view.action == "create":
-            return request.user.role in ("sales", "gestion")
+            return request.user.role in ("sales", "gestion")  # Only sales and gestion can create those objects.
         return request.user.role in ("sales", "gestion", "support")
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return request.user.role in ("sales", "support", "gestion")
         return view.queryset.filter(pk=obj.id, support=request.user).exists() or request.user.role == "gestion"
+        # Only the corresponding support or a gestion user can edit those objects.
 
 
 class IsManager(permissions.BasePermission):
